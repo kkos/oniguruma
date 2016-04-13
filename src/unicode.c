345b56b6b2401d1bf254d06601b66d83695c8419
kosako <kosako@sofnec.co.jp>
2016-04-13 13:38:23 +0900

    remove redundant code

--- src/unicode.c
+++ src/unicode.c
@@ -10830,12 +10830,14 @@ onigenc_unicode_mbc_case_fold(OnigEncoding enc,
 
 #ifdef USE_UNICODE_CASE_FOLD_TURKISH_AZERI
   if ((flag & ONIGENC_CASE_FOLD_TURKISH_AZERI) != 0) {
+    if (code == 0x0130) {
+      return ONIGENC_CODE_TO_MBC(enc, 0x0069, fold);
+    }
+#if 0
     if (code == 0x0049) {
       return ONIGENC_CODE_TO_MBC(enc, 0x0131, fold);
     }
-    else if (code == 0x0130) {
-      return ONIGENC_CODE_TO_MBC(enc, 0x0069, fold);
-    }
+#endif
   }
 #endif
 
@@ -10843,12 +10845,7 @@ onigenc_unicode_mbc_case_fold(OnigEncoding enc,
     if (to->n == 1) {
       return ONIGENC_CODE_TO_MBC(enc, to->code[0], fold);      
     }
-#if 0
-    /* NO NEEDS TO CHECK */
-    else if ((flag & INTERNAL_ONIGENC_CASE_FOLD_MULTI_CHAR) != 0) {
-#else
     else {
-#endif
       rlen = 0;
       for (i = 0; i < to->n; i++) {
         len = ONIGENC_CODE_TO_MBC(enc, to->code[i], fold);
