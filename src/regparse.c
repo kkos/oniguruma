a945d8ba8fb591451f8c8afc6129a96b4d101a01
Ruslan Kuprieiev <kupruser@gmail.com>
2017-06-15 22:57:47 +0300

    regparse: check xmalloc return value before using it
    "Unchecked return value of 'xmalloc' that
    is later used and can result in SEGFAULT."
    
    Id: 17
    
    Signed-off-by: Ruslan Kuprieiev <kupruser@gmail.com>

--- src/regparse.c
+++ src/regparse.c
@@ -422,6 +422,7 @@ onig_st_insert_strend(hash_table_type* table, const UChar* str_key,
   int result;
 
   key = (st_str_end_key* )xmalloc(sizeof(st_str_end_key));
+  CHECK_NULL_RETURN_MEMERR(key);
   key->s   = (UChar* )str_key;
   key->end = (UChar* )end_key;
   result = onig_st_insert(table, (st_data_t )key, value);
