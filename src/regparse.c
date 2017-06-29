cd56df2dfa2d73f8c153541aac637140340761ce
K.Kosako <kosako@sofnec.co.jp>
2017-06-29 14:27:49 +0900

    fix a memory leak

--- src/regparse.c
+++ src/regparse.c
@@ -159,7 +159,11 @@ bbuf_clone(BBuf** rto, BBuf* from)
   *rto = to = (BBuf* )xmalloc(sizeof(BBuf));
   CHECK_NULL_RETURN_MEMERR(to);
   r = BBUF_INIT(to, from->alloc);
-  if (r != 0) return r;
+  if (r != 0) {
+    xfree(to->p);
+    *rto = 0;
+    return r;
+  }
   to->used = from->used;
   xmemcpy(to->p, from->p, from->used);
   return 0;
