3465c066713ef886242c49806ff7e469954b3f73
K.Kosako <kosako@sofnec.co.jp>
2017-12-13 16:06:57 +0900

    check return value of concat_opt_exact()

--- src/regcomp.c
+++ src/regcomp.c
@@ -5230,24 +5230,28 @@ copy_opt_exact(OptExact* to, OptExact* from)
   *to = *from;
 }
 
-static void
+static int
 concat_opt_exact(OptExact* to, OptExact* add, OnigEncoding enc)
 {
-  int i, j, len;
+  int i, j, len, r;
   UChar *p, *end;
   OptAnc tanc;
 
   if (! to->ignore_case && add->ignore_case) {
-    if (to->len >= add->len) return ;  /* avoid */
+    if (to->len >= add->len) return 0;  /* avoid */
 
     to->ignore_case = 1;
   }
 
+  r = 0;
   p = add->s;
   end = p + add->len;
   for (i = to->len; p < end; ) {
     len = enclen(enc, p);
-    if (i + len > OPT_EXACT_MAXLEN) break;
+    if (i + len > OPT_EXACT_MAXLEN) {
+      r = 1; /* 1:full */
+      break;
+    }
     for (j = 0; j < len && p < end; j++)
       to->s[i++] = *p++;
   }
@@ -5258,6 +5262,8 @@ concat_opt_exact(OptExact* to, OptExact* add, OnigEncoding enc)
   concat_opt_anc_info(&tanc, &to->anc, &add->anc, 1, 1);
   if (! to->reach_end) tanc.right = 0;
   copy_opt_anc_info(&to->anc, &tanc);
+
+  return r;
 }
 
 static void
@@ -5817,7 +5823,8 @@ optimize_nodes(Node* node, NodeOpt* opt, OptEnv* env)
           if (nopt.exb.len > 0) {
             if (nopt.exb.reach_end) {
               for (i = 2; i <= qn->lower && ! is_full_opt_exact(&opt->exb); i++) {
-                concat_opt_exact(&opt->exb, &nopt.exb, enc);
+                int rc = concat_opt_exact(&opt->exb, &nopt.exb, enc);
+                if (rc > 0) break;
               }
               if (i < qn->lower) opt->exb.reach_end = 0;
             }
