d6f73b2340261a6fd8806dfc3544e64fe2f34899
K.Kosako <kosako@sofnec.co.jp>
2018-02-22 09:53:57 +0900

    fix: don't free default arg values in onig_free_reg_callout_list()

--- src/regparse.c
+++ src/regparse.c
@@ -2380,7 +2380,7 @@ onig_free_reg_callout_list(int n, CalloutListEntry* list)
 
   for (i = 0; i < n; i++) {
     if (list[i].of == ONIG_CALLOUT_OF_NAME) {
-      for (j = 0; j < list[i].u.arg.max_num; j++) {
+      for (j = 0; j < list[i].u.arg.passed_num; j++) {
         if (list[i].u.arg.types[j] == ONIG_TYPE_STRING) {
           if (IS_NOT_NULL(list[i].u.arg.vals[j].s.start))
             xfree(list[i].u.arg.vals[j].s.start);
