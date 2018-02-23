9610f009847c0dca017c58f95e256fb598ef263f
K.Kosako <kosako@sofnec.co.jp>
2018-02-23 17:48:44 +0900

    don't dup string st_insert_callout_name_table()

--- src/regparse.c
+++ src/regparse.c
@@ -1300,7 +1300,7 @@ i_free_callout_name_entry(st_callout_name_key* key, CalloutNameEntry* e,
                           void* arg ARG_UNUSED)
 {
   xfree(e->name);
-  xfree(key->s); /* is same as e->name */
+  /*xfree(key->s); */ /* is same as e->name */
   xfree(key);
   xfree(e);
   return ST_DELETE;
