254d93df9a8b31491c7d7c0c05d3132b41022866
K.Kosako <kkosako0@gmail.com>
2018-02-25 18:00:51 +0900

    fix return value of onig_get_callout_num_from_tag()

--- src/regparse.c
+++ src/regparse.c
@@ -1694,11 +1694,12 @@ onig_get_callout_num_from_tag(regex_t* reg,
   CalloutTagVal e;
 
   ext = REG_EXTP(reg);
-  if (IS_NULL(ext) || IS_NULL(ext->tag_table)) return -1;
+  if (IS_NULL(ext) || IS_NULL(ext->tag_table))
+    return ONIGERR_INVALID_CALLOUT_TAG_NAME;
 
   r = onig_st_lookup_strend(ext->tag_table, tag, tag_end,
 			    (HashDataType* )((void* )(&e)));
-  if (r == 0) return -1;
+  if (r == 0) return ONIGERR_INVALID_CALLOUT_TAG_NAME;
   return (int )e;
 }
 
