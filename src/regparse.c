b74f73a829d7d2e68062c86040e4b35aadf3987b
K.Kosako <kosako@sofnec.co.jp>
2018-02-28 16:51:51 +0900

    strict check in is_allowed_callout_name()

--- src/regparse.c
+++ src/regparse.c
@@ -1442,11 +1442,17 @@ is_allowed_callout_name(OnigEncoding enc, UChar* name, UChar* name_end)
   UChar* p;
   OnigCodePoint c;
 
+  if (name >= name_end) return 0;
+
   p = name;
   while (p < name_end) {
     c = ONIGENC_MBC_TO_CODE(enc, p, name_end);
     if (! IS_ALLOWED_CODE_IN_CALLOUT_NAME(c))
       return 0;
+
+    if (p == name) {
+      if (c >= '0' && c <= '9') return 0;
+    }
 
     p += ONIGENC_MBC_ENC_LEN(enc, p);
   }
