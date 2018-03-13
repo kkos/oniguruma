a86118fdd538d4355d746536a448e94eba887783
K.Kosako <kosako@sofnec.co.jp>
2018-03-13 18:04:49 +0900

    fix: index of callout list entry is callout num - 1

--- src/regparse.c
+++ src/regparse.c
@@ -1707,9 +1707,11 @@ typedef intptr_t   CalloutTagVal;
 static int
 i_callout_callout_list_set(UChar* key, CalloutTagVal e, void* arg)
 {
+  int num;
   RegexExt* ext = (RegexExt* )arg;
 
-  ext->callout_list[e].flag |= CALLOUT_TAG_LIST_FLAG_TAG_EXIST;
+  num = (int )e - 1;
+  ext->callout_list[num].flag |= CALLOUT_TAG_LIST_FLAG_TAG_EXIST;
   return ST_CONTINUE;
 }
 
