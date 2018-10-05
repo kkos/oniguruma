5096808308ef194082f9803cbfdb11b0f14a026d
K.Kosako <kosako@sofnec.co.jp>
2018-10-05 17:17:52 +0900

    speed improvement of sunday_quick_search_step_forward()

--- src/regexec.c
+++ src/regexec.c
@@ -4124,11 +4124,14 @@ sunday_quick_search_step_forward(regex_t* reg,
   const UChar *tail;
   int skip, tlen1;
   int map_offset;
+  OnigEncoding enc;
 
 #ifdef ONIG_DEBUG_SEARCH
   fprintf(stderr,
           "sunday_quick_search_step_forward: text: %p, text_end: %p, text_range: %p\n", text, text_end, text_range);
 #endif
+
+  enc = reg->enc;
 
   tail = target_end - 1;
   tlen1 = (int )(tail - target);
@@ -4148,10 +4151,16 @@ sunday_quick_search_step_forward(regex_t* reg,
     }
     if (se + map_offset >= text_end) break;
     skip = reg->map[*(se + map_offset)];
+#if 0
     t = s;
     do {
-      s += enclen(reg->enc, s);
+      s += enclen(enc, s);
     } while ((s - t) < skip && s < end);
+#else
+    s += skip;
+    if (s < end)
+      s = onigenc_get_right_adjust_char_head(enc, text, s);
+#endif
   }
 
   return (UChar* )NULL;
