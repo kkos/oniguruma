fa4a4af364a6e28d153e3ca74e995cbf24bb9611
K.Kosako <kkosako0@gmail.com>
2019-03-29 22:39:08 +0900

    remove tabs

--- contributed/libfuzzer-onig.cpp
+++ contributed/libfuzzer-onig.cpp
@@ -3,14 +3,14 @@
 
 Usage:
 * compile oniguruma with something like
-	./configure CC=clang LD=clang CFLAGS="-fsanitize-coverage=edge -fsanitize=address" \
-		LDFLAGS="-fsanitize-coverage=edge -fsanitize=address"
+    ./configure CC=clang LD=clang CFLAGS="-fsanitize-coverage=edge -fsanitize=address" \
+    LDFLAGS="-fsanitize-coverage=edge -fsanitize=address"
 * Compile libfuzzer stub and link against static libonig.a and libFuzzer.a:
-	clang++ libfuzzer-onig.cpp src/.libs/libonig.a libFuzzer.a -o libfuzzer-onig \
-		-fsanitize-coverage=edge -fsanitize=address
+    clang++ libfuzzer-onig.cpp src/.libs/libonig.a libFuzzer.a -o libfuzzer-onig \
+    -fsanitize-coverage=edge -fsanitize=address
 * Put sample patterns in directory "in/"
 * Run
-	./libfuzzer-onig in
+    ./libfuzzer-onig in
 
 Consult libfuzzer docs for further details and how to create libFuzzer.a:
 http://llvm.org/docs/LibFuzzer.html
@@ -22,7 +22,7 @@ http://llvm.org/docs/LibFuzzer.html
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t * Data, size_t Size)
 {
-	regex_t *reg;
+  regex_t *reg;
   OnigEncoding enc;
 
   enc = ONIG_ENCODING_UTF8;
@@ -31,13 +31,13 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t * Data, size_t Size)
   onig_initialize(&enc, 1);
 #endif
 
-	if (onig_new(&reg, Data, Data + Size, ONIG_OPTION_DEFAULT, enc,
+  if (onig_new(&reg, Data, Data + Size, ONIG_OPTION_DEFAULT, enc,
                ONIG_SYNTAX_DEFAULT, 0) == 0)
-		onig_free(reg);
+    onig_free(reg);
 
 #ifdef FULL_TEST
   onig_end();
 #endif
 
-	return 0;
+  return 0;
 }
