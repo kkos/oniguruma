544787b9c3ba79ec4fd2bc53636113627baee277
K.Kosako <kosako@sofnec.co.jp>
2019-04-01 16:43:54 +0900

    setup 6.9.2

--- src/oniguruma.h
+++ src/oniguruma.h
@@ -36,9 +36,9 @@ extern "C" {
 #define ONIGURUMA
 #define ONIGURUMA_VERSION_MAJOR   6
 #define ONIGURUMA_VERSION_MINOR   9
-#define ONIGURUMA_VERSION_TEENY   1
+#define ONIGURUMA_VERSION_TEENY   2
 
-#define ONIGURUMA_VERSION_INT     60901
+#define ONIGURUMA_VERSION_INT     60902
 
 #ifndef P_
 #if defined(__STDC__) || defined(_WIN32)
