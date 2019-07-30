2a6ea80fa056a23629c044eb263166a30f5a374f
K.Kosako <kosako@sofnec.co.jp>
2019-07-30 15:04:37 +0900

    refactoring

--- build_harnesses.sh
+++ build_harnesses.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 make clean
-#autoreconf -vfi
+autoreconf -vfi
 # build the library with ASAN
 ./configure CC=clang LD=clang CFLAGS="-g -fsanitize=address -fno-omit-frame-pointer -fsanitize=fuzzer-no-link" LDFLAGS="-g -fsanitize=address -fno-omit-frame-pointer -fsanitize=fuzzer-no-link"
 make -j4
@@ -12,15 +12,17 @@ LIBFUZZER_FLAGS="-fsanitize=fuzzer,address -fno-omit-frame-pointer"
 #LIBS="src/.libs/libonig.a"
 LIBS="src/.libs/libonig.a /usr/local/lib/libLLVMFuzzerMain.a"
 
-# Libfuzzer builds
-clang++ contributed/libfuzzer-onig.cpp $LIBS -Isrc/ -g $LIBFUZZER_FLAGS -o $OUT/libfuzzer-onig
-clang harnesses/syntax-harness.c $LIBS -Isrc -g $LIBFUZZER_FLAGS -o $OUT/syntax-libfuzzer
-clang harnesses/encode-harness.c $LIBS -Isrc -g $LIBFUZZER_FLAGS -o $OUT/encode-libfuzzer
-clang harnesses/deluxe-encode-harness.c $LIBS -Isrc -g $LIBFUZZER_FLAGS -o $OUT/deluxe-encode-libfuzzer
+CFLAGS="-Isrc -g $LIBFUZZER_FLAGS"
 
-clang -DUTF16_BE harnesses/encode-harness.c $LIBS -Isrc -g $LIBFUZZER_FLAGS -o $OUT/utf16-be-libfuzzer
-clang -DUTF16_LE harnesses/encode-harness.c $LIBS -Isrc -g $LIBFUZZER_FLAGS -o $OUT/utf16-le-libfuzzer
-clang -DWITH_READ_MAIN harnesses/encode-harness.c src/.libs/libonig.a -Isrc -g $LIBFUZZER_FLAGS -o $OUT/main-encode
-clang -DWITH_READ_MAIN -DUTF16_LE harnesses/encode-harness.c src/.libs/libonig.a -Isrc -g $LIBFUZZER_FLAGS -o $OUT/main-utf16-le
-clang -DWITH_READ_MAIN -DUTF16_BE harnesses/encode-harness.c src/.libs/libonig.a -Isrc -g $LIBFUZZER_FLAGS -o $OUT/main-utf16-be
-clang -DWITH_READ_MAIN harnesses/deluxe-encode-harness.c $LIBS -Isrc -g $LIBFUZZER_FLAGS -o $OUT/main-deluxe-encode
+# Libfuzzer builds
+clang++ contributed/libfuzzer-onig.cpp $LIBS $CFLAGS -o $OUT/libfuzzer-onig
+clang harnesses/syntax-harness.c $LIBS $CFLAGS -o $OUT/syntax-libfuzzer
+clang harnesses/encode-harness.c $LIBS $CFLAGS -o $OUT/encode-libfuzzer
+clang harnesses/deluxe-encode-harness.c $LIBS $CFLAGS -o $OUT/deluxe-encode-libfuzzer
+
+clang -DUTF16_BE harnesses/encode-harness.c $LIBS $CFLAGS -o $OUT/utf16-be-libfuzzer
+clang -DUTF16_LE harnesses/encode-harness.c $LIBS $CFLAGS -o $OUT/utf16-le-libfuzzer
+clang -DWITH_READ_MAIN harnesses/encode-harness.c src/.libs/libonig.a $CFLAGS -o $OUT/main-encode
+clang -DWITH_READ_MAIN -DUTF16_LE harnesses/encode-harness.c src/.libs/libonig.a $CFLAGS -o $OUT/main-utf16-le
+clang -DWITH_READ_MAIN -DUTF16_BE harnesses/encode-harness.c src/.libs/libonig.a $CFLAGS -o $OUT/main-utf16-be
+clang -DWITH_READ_MAIN harnesses/deluxe-encode-harness.c $LIBS $CFLAGS -o $OUT/main-deluxe-encode
