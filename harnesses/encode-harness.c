6abc057c150ba41a87a7b94e1aacbb11694f07e1
K.Kosako <kosako@sofnec.co.jp>
2019-08-09 10:56:53 +0900

    add some encodings into encode-harness.c

--- harnesses/encode-harness.c
+++ harnesses/encode-harness.c
@@ -109,7 +109,9 @@ int LLVMFuzzerTestOneInput(const uint8_t * Data, size_t Size)
 	  ONIG_ENCODING_ISO_8859_1,
 	  ONIG_ENCODING_UTF8,
     ONIG_ENCODING_KOI8_R,
-    ONIG_ENCODING_BIG5
+    ONIG_ENCODING_BIG5,
+    ONIG_ENCODING_GB18030,
+    ONIG_ENCODING_EUC_TW
   };
 
   unsigned char encoding_choice;
