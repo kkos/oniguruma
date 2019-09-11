cd4a0a87670765f895fb7c54be68ebbeaa64fd4f
K.Kosako <kosako@sofnec.co.jp>
2019-09-11 12:26:35 +0900

    don't set parse depth limit

--- harnesses/encode-harness.c
+++ harnesses/encode-harness.c
@@ -13,7 +13,7 @@
 #include "oniguruma.h"
 
 
-#define PARSE_DEPTH_LIMIT   120
+//#define PARSE_DEPTH_LIMIT   120
 #define RETRY_LIMIT        3500
 
 typedef unsigned char uint8_t;
@@ -76,7 +76,7 @@ exec(OnigEncoding enc, OnigOptionType options,
 
   onig_initialize(&enc, 1);
   onig_set_retry_limit_in_match(RETRY_LIMIT);
-  onig_set_parse_depth_limit(PARSE_DEPTH_LIMIT);
+  //onig_set_parse_depth_limit(PARSE_DEPTH_LIMIT);
 
   r = onig_new(&reg, pattern, pattern_end,
                options, enc, ONIG_SYNTAX_DEFAULT, &einfo);
