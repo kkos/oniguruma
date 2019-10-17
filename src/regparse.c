bad4d29274323489613f519fdfbc36bcc7b673bc
K.Kosako <kosako@sofnec.co.jp>
2019-10-17 16:53:39 +0900

    add onig_new_cclass_with_code_list()

--- src/regparse.c
+++ src/regparse.c
@@ -7757,6 +7757,29 @@ clear_not_flag_cclass(CClassNode* cc, OnigEncoding enc)
   }\
 } while (0)
 
+extern int
+onig_new_cclass_with_code_list(Node** rnode, OnigEncoding enc,
+                               int n, OnigCodePoint codes[])
+{
+  int i;
+  Node* node;
+  CClassNode* cc;
+
+  *rnode = NULL_NODE;
+
+  node = node_new_cclass();
+  CHECK_NULL_RETURN_MEMERR(node);
+
+  cc = CCLASS_(node);
+
+  for (i = 0; i < n; i++) {
+    ADD_CODE_INTO_CC(cc, codes[i], enc);
+  }
+
+  *rnode = node;
+  return 0;
+}
+
 typedef struct {
   ScanEnv*    env;
   CClassNode* cc;
