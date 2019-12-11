0e7141e15508e7913fe6e58c8ebaf5d8c6eb73d7
K.Kosako <kosako@sofnec.co.jp>
2019-12-11 17:09:17 +0900

    don't change options value in compile time

--- src/regcomp.c
+++ src/regcomp.c
@@ -1353,11 +1353,8 @@ static int
 compile_length_option_node(BagNode* node, regex_t* reg)
 {
   int tlen;
-  OnigOptionType prev = reg->options;
 
-  reg->options = node->o.options;
   tlen = compile_length_tree(NODE_BAG_BODY(node), reg);
-  reg->options = prev;
 
   return tlen;
 }
@@ -1366,11 +1363,8 @@ static int
 compile_option_node(BagNode* node, regex_t* reg, ScanEnv* env)
 {
   int r;
-  OnigOptionType prev = reg->options;
 
-  reg->options = node->o.options;
   r = compile_tree(NODE_BAG_BODY(node), reg, env);
-  reg->options = prev;
 
   return r;
 }
