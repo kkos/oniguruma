ea8f01fa50f6b7f5a88b10052f06d914693fe8af
K.Kosako <kosako@sofnec.co.jp>
2019-12-24 10:44:25 +0900

    refactoring

--- src/regcomp.c
+++ src/regcomp.c
@@ -586,6 +586,177 @@ unset_addr_list_add(UnsetAddrList* list, int offset, struct _Node* node)
 #endif /* USE_CALL */
 
 
+#define GET_CHAR_LEN_VARLEN           -1
+#define GET_CHAR_LEN_TOP_ALT_VARLEN   -2
+
+/* fixed size pattern node only */
+static int
+node_char_len1(Node* node, regex_t* reg, int* len, int level)
+{
+  int tlen;
+  int r = 0;
+
+  level++;
+  *len = 0;
+  switch (NODE_TYPE(node)) {
+  case NODE_LIST:
+    do {
+      r = node_char_len1(NODE_CAR(node), reg, &tlen, level);
+      if (r == 0)
+        *len = distance_add(*len, tlen);
+    } while (r == 0 && IS_NOT_NULL(node = NODE_CDR(node)));
+    break;
+
+  case NODE_ALT:
+    {
+      int tlen2;
+      int varlen = 0;
+
+      r = node_char_len1(NODE_CAR(node), reg, &tlen, level);
+      while (r == 0 && IS_NOT_NULL(node = NODE_CDR(node))) {
+        r = node_char_len1(NODE_CAR(node), reg, &tlen2, level);
+        if (r == 0) {
+          if (tlen != tlen2)
+            varlen = 1;
+        }
+      }
+      if (r == 0) {
+        if (varlen != 0) {
+          if (level == 1)
+            r = GET_CHAR_LEN_TOP_ALT_VARLEN;
+          else
+            r = GET_CHAR_LEN_VARLEN;
+        }
+        else
+          *len = tlen;
+      }
+    }
+    break;
+
+  case NODE_STRING:
+    {
+      StrNode* sn = STR_(node);
+      UChar *s = sn->s;
+
+      /* Ignore IGNORECASE option flag.
+         Because node_char_len() is used for
+         look-behind/look-behind-not only.
+       */
+      while (s < sn->end) {
+        s += enclen(reg->enc, s);
+        (*len)++;
+      }
+    }
+    break;
+
+  case NODE_QUANT:
+    {
+      QuantNode* qn = QUANT_(node);
+
+      if (qn->lower == qn->upper) {
+        if (qn->upper == 0) {
+          *len = 0;
+        }
+        else {
+          r = node_char_len1(NODE_BODY(node), reg, &tlen, level);
+          if (r == 0)
+            *len = distance_multiply(tlen, qn->lower);
+        }
+      }
+      else
+        r = GET_CHAR_LEN_VARLEN;
+    }
+    break;
+
+#ifdef USE_CALL
+  case NODE_CALL:
+    if (! NODE_IS_RECURSION(node))
+      r = node_char_len1(NODE_BODY(node), reg, len, level);
+    else
+      r = GET_CHAR_LEN_VARLEN;
+    break;
+#endif
+
+  case NODE_CTYPE:
+  case NODE_CCLASS:
+    *len = 1;
+    break;
+
+  case NODE_BAG:
+    {
+      BagNode* en = BAG_(node);
+
+      switch (en->type) {
+      case BAG_MEMORY:
+#ifdef USE_CALL
+        if (NODE_IS_FIXED_CLEN(node))
+          *len = en->char_len;
+        else {
+          r = node_char_len1(NODE_BODY(node), reg, len, level);
+          if (r == 0) {
+            en->char_len = *len;
+            NODE_STATUS_ADD(node, FIXED_CLEN);
+          }
+        }
+        break;
+#endif
+      case BAG_OPTION:
+      case BAG_STOP_BACKTRACK:
+        r = node_char_len1(NODE_BODY(node), reg, len, level);
+        break;
+      case BAG_IF_ELSE:
+        {
+          int clen, elen;
+
+          r = node_char_len1(NODE_BODY(node), reg, &clen, level);
+          if (r == 0) {
+            if (IS_NOT_NULL(en->te.Then)) {
+              r = node_char_len1(en->te.Then, reg, &tlen, level);
+              if (r != 0) break;
+            }
+            else tlen = 0;
+            if (IS_NOT_NULL(en->te.Else)) {
+              r = node_char_len1(en->te.Else, reg, &elen, level);
+              if (r != 0) break;
+            }
+            else elen = 0;
+
+            if (clen + tlen != elen) {
+              r = GET_CHAR_LEN_VARLEN;
+            }
+            else {
+              *len = elen;
+            }
+          }
+        }
+        break;
+      }
+    }
+    break;
+
+  case NODE_ANCHOR:
+  case NODE_GIMMICK:
+    break;
+
+  case NODE_BACKREF:
+    if (NODE_IS_CHECKER(node))
+      break;
+    /* fall */
+  default:
+    r = GET_CHAR_LEN_VARLEN;
+    break;
+  }
+
+  return r;
+}
+
+static int
+node_char_len(Node* node, regex_t* reg, int* len)
+{
+  return node_char_len1(node, reg, len, 0);
+}
+
+
 static int
 add_op(regex_t* reg, int opcode)
 {
@@ -1396,8 +1567,6 @@ compile_length_bag_node(BagNode* node, regex_t* reg)
 
   return len;
 }
-
-static int node_char_len(Node* node, regex_t* reg, int* len);
 
 static int
 compile_bag_memory_node(BagNode* node, regex_t* reg, ScanEnv* env)
@@ -2393,177 +2562,6 @@ fix_unset_addr_list(UnsetAddrList* uslist, regex_t* reg)
   return 0;
 }
 #endif
-
-
-#define GET_CHAR_LEN_VARLEN           -1
-#define GET_CHAR_LEN_TOP_ALT_VARLEN   -2
-
-/* fixed size pattern node only */
-static int
-node_char_len1(Node* node, regex_t* reg, int* len, int level)
-{
-  int tlen;
-  int r = 0;
-
-  level++;
-  *len = 0;
-  switch (NODE_TYPE(node)) {
-  case NODE_LIST:
-    do {
-      r = node_char_len1(NODE_CAR(node), reg, &tlen, level);
-      if (r == 0)
-        *len = distance_add(*len, tlen);
-    } while (r == 0 && IS_NOT_NULL(node = NODE_CDR(node)));
-    break;
-
-  case NODE_ALT:
-    {
-      int tlen2;
-      int varlen = 0;
-
-      r = node_char_len1(NODE_CAR(node), reg, &tlen, level);
-      while (r == 0 && IS_NOT_NULL(node = NODE_CDR(node))) {
-        r = node_char_len1(NODE_CAR(node), reg, &tlen2, level);
-        if (r == 0) {
-          if (tlen != tlen2)
-            varlen = 1;
-        }
-      }
-      if (r == 0) {
-        if (varlen != 0) {
-          if (level == 1)
-            r = GET_CHAR_LEN_TOP_ALT_VARLEN;
-          else
-            r = GET_CHAR_LEN_VARLEN;
-        }
-        else
-          *len = tlen;
-      }
-    }
-    break;
-
-  case NODE_STRING:
-    {
-      StrNode* sn = STR_(node);
-      UChar *s = sn->s;
-
-      /* Ignore IGNORECASE option flag.
-         Because node_char_len() is used for
-         look-behind/look-behind-not only.
-       */
-      while (s < sn->end) {
-        s += enclen(reg->enc, s);
-        (*len)++;
-      }
-    }
-    break;
-
-  case NODE_QUANT:
-    {
-      QuantNode* qn = QUANT_(node);
-
-      if (qn->lower == qn->upper) {
-        if (qn->upper == 0) {
-          *len = 0;
-        }
-        else {
-          r = node_char_len1(NODE_BODY(node), reg, &tlen, level);
-          if (r == 0)
-            *len = distance_multiply(tlen, qn->lower);
-        }
-      }
-      else
-        r = GET_CHAR_LEN_VARLEN;
-    }
-    break;
-
-#ifdef USE_CALL
-  case NODE_CALL:
-    if (! NODE_IS_RECURSION(node))
-      r = node_char_len1(NODE_BODY(node), reg, len, level);
-    else
-      r = GET_CHAR_LEN_VARLEN;
-    break;
-#endif
-
-  case NODE_CTYPE:
-  case NODE_CCLASS:
-    *len = 1;
-    break;
-
-  case NODE_BAG:
-    {
-      BagNode* en = BAG_(node);
-
-      switch (en->type) {
-      case BAG_MEMORY:
-#ifdef USE_CALL
-        if (NODE_IS_FIXED_CLEN(node))
-          *len = en->char_len;
-        else {
-          r = node_char_len1(NODE_BODY(node), reg, len, level);
-          if (r == 0) {
-            en->char_len = *len;
-            NODE_STATUS_ADD(node, FIXED_CLEN);
-          }
-        }
-        break;
-#endif
-      case BAG_OPTION:
-      case BAG_STOP_BACKTRACK:
-        r = node_char_len1(NODE_BODY(node), reg, len, level);
-        break;
-      case BAG_IF_ELSE:
-        {
-          int clen, elen;
-
-          r = node_char_len1(NODE_BODY(node), reg, &clen, level);
-          if (r == 0) {
-            if (IS_NOT_NULL(en->te.Then)) {
-              r = node_char_len1(en->te.Then, reg, &tlen, level);
-              if (r != 0) break;
-            }
-            else tlen = 0;
-            if (IS_NOT_NULL(en->te.Else)) {
-              r = node_char_len1(en->te.Else, reg, &elen, level);
-              if (r != 0) break;
-            }
-            else elen = 0;
-
-            if (clen + tlen != elen) {
-              r = GET_CHAR_LEN_VARLEN;
-            }
-            else {
-              *len = elen;
-            }
-          }
-        }
-        break;
-      }
-    }
-    break;
-
-  case NODE_ANCHOR:
-  case NODE_GIMMICK:
-    break;
-
-  case NODE_BACKREF:
-    if (NODE_IS_CHECKER(node))
-      break;
-    /* fall */
-  default:
-    r = GET_CHAR_LEN_VARLEN;
-    break;
-  }
-
-  return r;
-}
-
-static int
-node_char_len(Node* node, regex_t* reg, int* len)
-{
-  return node_char_len1(node, reg, len, 0);
-}
 
 /* x is not included y ==>  1 : 0 */
 static int
