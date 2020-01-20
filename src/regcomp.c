ff5dc4b3ae664ab768e23e344f0dc04824c7c96f
K.Kosako <kosako@sofnec.co.jp>
2020-01-20 14:27:36 +0900

    forbid use of absent stopper/clear in look-behind

--- src/regcomp.c
+++ src/regcomp.c
@@ -3205,6 +3205,70 @@ get_head_value_node(Node* node, int exact, regex_t* reg)
   return n;
 }
 
+static int
+check_called_node_in_look_behind(Node* node, int not)
+{
+  int r;
+
+  r = 0;
+
+  switch (NODE_TYPE(node)) {
+  case NODE_LIST:
+  case NODE_ALT:
+    do {
+      r = check_called_node_in_look_behind(NODE_CAR(node), not);
+    } while (r == 0 && IS_NOT_NULL(node = NODE_CDR(node)));
+    break;
+
+  case NODE_QUANT:
+    r = check_called_node_in_look_behind(NODE_BODY(node), not);
+    break;
+
+  case NODE_BAG:
+    {
+      BagNode* en = BAG_(node);
+
+      if (en->type == BAG_MEMORY) {
+        if (NODE_IS_MARK1(node))
+          return 0;
+        else {
+          NODE_STATUS_ADD(node, MARK1);
+          r = check_called_node_in_look_behind(NODE_BODY(node), not);
+          NODE_STATUS_REMOVE(node, MARK1);
+        }
+      }
+      else {
+        r = check_called_node_in_look_behind(NODE_BODY(node), not);
+        if (r == 0 && en->type == BAG_IF_ELSE) {
+          if (IS_NOT_NULL(en->te.Then)) {
+            r = check_called_node_in_look_behind(en->te.Then, not);
+            if (r != 0) break;
+          }
+          if (IS_NOT_NULL(en->te.Else)) {
+            r = check_called_node_in_look_behind(en->te.Else, not);
+          }
+        }
+      }
+    }
+    break;
+
+  case NODE_ANCHOR:
+    if (IS_NOT_NULL(NODE_BODY(node)))
+      r = check_called_node_in_look_behind(NODE_BODY(node), not);
+    break;
+
+  case NODE_GIMMICK:
+    if (NODE_IS_ABSENT_WITH_SIDE_EFFECTS(node) != 0)
+      return 1;
+    break;
+
+  default:
+    break;
+  }
+
+  return r;
+}
+
 /* allowed node types in look-behind */
 #define ALLOWED_TYPE_IN_LB \
   ( NODE_BIT_LIST | NODE_BIT_ALT | NODE_BIT_STRING | NODE_BIT_CCLASS \
@@ -3284,6 +3348,14 @@ check_node_in_look_behind(Node* node, int not)
     break;
 
   case NODE_GIMMICK:
+    if (NODE_IS_ABSENT_WITH_SIDE_EFFECTS(node) != 0)
+      return 1;
+    break;
+
+  case NODE_CALL:
+    r = check_called_node_in_look_behind(NODE_BODY(node), not);
+    break;
+
   default:
     break;
   }
