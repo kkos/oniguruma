8d08987155ac974a252f622e6a59fe3dbb3e71df
K.Kosako <kosako@sofnec.co.jp>
2020-02-10 14:53:50 +0900

    fix: CASE called node doesn't have called address

--- src/regcomp.c
+++ src/regcomp.c
@@ -3006,8 +3006,18 @@ fix_unset_addr_list(UnsetAddrList* uslist, regex_t* reg)
   AbsAddrType* paddr;
 
   for (i = 0; i < uslist->num; i++) {
-    if (! NODE_IS_FIXED_ADDR(uslist->us[i].target))
-      return ONIGERR_PARSER_BUG;
+    if (! NODE_IS_FIXED_ADDR(uslist->us[i].target)) {
+      if (NODE_IS_CALLED(uslist->us[i].target))
+        return ONIGERR_PARSER_BUG;
+      else {
+        /* CASE: called node doesn't have called address.
+           ex. /((|a\g<1>)(.){0}){0}\g<3>/
+           group-1 doesn't called, but compiled into bytecodes,
+           because group-3 is referred from outside.
+        */
+        continue;
+      }
+    }
 
     en = BAG_(uslist->us[i].target);
     addr   = en->m.called_addr;
