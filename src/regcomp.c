edad2a332549068dac07e114fd350a999b2ba0b4
K.Kosako <kkosako0@gmail.com>
2020-04-23 22:03:25 +0900

    fix #192 (recorrect): Unexpected regex match

--- src/regcomp.c
+++ src/regcomp.c
@@ -656,8 +656,11 @@ static void
 mmcl_alt_merge(MinMaxCharLen* to, MinMaxCharLen* alt)
 {
   if (to->min > alt->min) {
-    to->min = alt->min;
-    if (alt->min_is_sure != 0)
+    to->min         = alt->min;
+    to->min_is_sure = alt->min_is_sure;
+  }
+  else if (to->min == alt->min) {
+    if (alt->min_is_sure != FALSE)
       to->min_is_sure = TRUE;
   }
 
