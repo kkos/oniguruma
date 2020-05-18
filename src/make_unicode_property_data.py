e3bec28f0f2e7a15cf8add51b46505abebd428be
K.Kosako <kkosako0@gmail.com>
2020-05-18 21:46:33 +0900

    remove internal index numbers from UNICODE_PROPERTIES file

--- src/make_unicode_property_data.py
+++ src/make_unicode_property_data.py
@@ -405,7 +405,7 @@ def set_max_prop_name(name):
 def entry_prop_name(name, index):
   set_max_prop_name(name)
   if OUTPUT_LIST_MODE and index >= len(POSIX_LIST):
-    print >> UPF, "%3d: %s" % (index, name)
+    print >> UPF, "%s" % (name)
 
 def entry_and_print_prop_and_index(name, index):
   entry_prop_name(name, index)
