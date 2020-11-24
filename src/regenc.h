3777c8627848536e1c5a9d3e81aee8fa6e844f06
K.Kosako <kkosako0@gmail.com>
2020-11-24 11:51:15 +0900

    refactoring

--- src/regenc.h
+++ src/regenc.h
@@ -202,12 +202,12 @@ extern int onigenc_wb_is_break_position P_((OnigEncoding enc, UChar* p, UChar* p
 #define FOLDS1_UNFOLDS_NUM(i)  (OnigUnicodeFolds1[(i)+1])
 #define FOLDS2_UNFOLDS_NUM(i)  (OnigUnicodeFolds2[(i)+2])
 #define FOLDS3_UNFOLDS_NUM(i)  (OnigUnicodeFolds3[(i)+3])
-#define FOLDS1_UNFOLDS(i)      (OnigUnicodeFolds1 + (i) + 2)
-#define FOLDS2_UNFOLDS(i)      (OnigUnicodeFolds2 + (i) + 3)
-#define FOLDS3_UNFOLDS(i)      (OnigUnicodeFolds3 + (i) + 4)
-#define FOLDS1_NEXT_INDEX(i)   ((i) + 2 + OnigUnicodeFolds1[(i)+1])
-#define FOLDS2_NEXT_INDEX(i)   ((i) + 3 + OnigUnicodeFolds2[(i)+2])
-#define FOLDS3_NEXT_INDEX(i)   ((i) + 4 + OnigUnicodeFolds3[(i)+3])
+#define FOLDS1_UNFOLDS(i)      (FOLDS1_FOLD(i) + 2)
+#define FOLDS2_UNFOLDS(i)      (FOLDS2_FOLD(i) + 3)
+#define FOLDS3_UNFOLDS(i)      (FOLDS3_FOLD(i) + 4)
+#define FOLDS1_NEXT_INDEX(i)   ((i) + 2 + FOLDS1_UNFOLDS_NUM(i))
+#define FOLDS2_NEXT_INDEX(i)   ((i) + 3 + FOLDS2_UNFOLDS_NUM(i))
+#define FOLDS3_NEXT_INDEX(i)   ((i) + 4 + FOLDS3_UNFOLDS_NUM(i))
 
 #define FOLDS_FOLD_ADDR_BUK(buk, addr) do {\
   if ((buk)->fold_len == 1)\
