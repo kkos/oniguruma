3f966829ee4350682428d4269f46d8099adf29e1
K.Kosako <kkosako0@gmail.com>
2025-04-03 10:55:04 +0900

    more strict check for invalid encooded byte value in utf-8

--- src/utf8.c
+++ src/utf8.c
@@ -2,7 +2,7 @@
   utf8.c -  Oniguruma (regular expression library)
 **********************************************************************/
 /*-
- * Copyright (c) 2002-2019  K.Kosako
+ * Copyright (c) 2002-2025  K.Kosako
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -79,7 +79,7 @@ is_valid_mbc_string(const UChar* p, const UChar* end)
   int i, len;
 
   while (p < end) {
-    if (! utf8_islead(*p))
+    if (*p > 0xf4 || (*p > 0x7f && *p < 0xc2))
       return FALSE;
 
     len = mbc_enc_len(p++);
